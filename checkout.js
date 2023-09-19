28c5ad9ccd01e9ce40b091e2f6cf36f563353d79
rafaelbrian <80716895+sunnyrafael@users.noreply.github.com>
2023-09-19 02:06:47 -0300

    Create checkout.js

---
+++ checkout.js
@@ -0,0 +1,17 @@
+import {
+  desenharProdutoCarrinhoSimples,
+  lerLocalStorage,
+} from "./src/utilidades";
+
+function desenharProdutosCheckout() {
+  const idsProdutoCarrinhoComQuantidade = lerLocalStorage("carrinho");
+  for (const idProduto in idsProdutoCarrinhoComQuantidade) {
+    desenharProdutoCarrinhoSimples(
+      idProduto,
+      "container-produtos-checkout",
+      idsProdutoCarrinhoComQuantidade[idProduto]
+    );
+  }
+}
+
+desenharProdutosCheckout();
